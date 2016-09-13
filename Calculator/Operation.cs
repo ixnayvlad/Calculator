83218a52975d0181ea1b3023775f3f761f495428
ixnayvlad <ixnayvlad@gmail.com>
2016-09-13 14:04:04 +0300

    Calculator Commit

---
+++ Calculator/Operation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Operation
+    {
+        Operators operation = new Operators();
+        int a;
+        int b;
+        char operationChar;
+        int result;
+
+        public int NewOperation()
+        {
+            result = 0;
+            Console.WriteLine("Первое число:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Второе число:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Операция:");
+            operationChar = Console.ReadLine()[0];
+            
+            switch (operationChar)
+            {
+            case '+': result = operation.Sum(a, b); break;
+            case '-': result = operation.Sub(a, b); break;
+            case '*': result = operation.Mul(a, b); break;
+            case '/': result = operation.Div(a, b); break;
+            }
+            
+            
+            return result;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) 
+            {
+                Operation A = new Operation();
+                Console.WriteLine("Результат: \n" + A.NewOperation() + "\n");
+            }
+        }
+    }
+}
